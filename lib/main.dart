1a2e980049805c806d6c1493dbb1f3bd0007a898
shallagehmasood <shallagehmasood@gmail.com>
2025-10-28 22:32:02 +0330

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'screens/login_screen.dart';
 import 'screens/main_screen.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  final prefs = await SharedPreferences.getInstance();
-  final userId = prefs.getString('user_id');
-
-  runApp(MyApp(initialUserId: userId));
+void main() {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final String? initialUserId;
-
-  const MyApp({super.key, this.initialUserId});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -25,9 +17,9 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       ),
-      home: initialUserId != null ? MainScreen(userId: initialUserId!) : LoginScreen(),
       locale: const Locale('fa', 'IR'),
       supportedLocales: const [Locale('fa', 'IR')],
+      home: const MainScreen(userId: "demo_user"),
     );
   }
 }
