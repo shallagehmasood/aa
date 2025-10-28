b09abdc50824be25ff2db3a147ac89ba409be9df
shallagehmasood <shallagehmasood@gmail.com>
2025-10-28 16:58:27 +0330

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 android {
     namespace = "ir.masoodfx.alertx"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973" // برای رفع خطای NDK
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
